5a1bc391e431ef236b1d2e055fb6dc72d30a741e
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-01-30 11:50:11 +0100

    first commit

---
+++ practica_1.js
@@ -0,0 +1,15 @@
+const showName=(name)=>{
+    console.log(name)
+}
+
+showName('Juan');
+
+function ShowYou (pre, last){
+  console.log(`Mi nombre es ${pre} y mi apellido es ${last}`)  
+}
+ShowYou('David', 'Flaquer');
+
+let nombre ='Pitilongui';
+
+console.log( 'Hola '+ nombre);
+console.log ( `Hola ${ nombre }`);
