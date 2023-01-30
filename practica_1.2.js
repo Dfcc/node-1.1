647b9fb2a56dd48622a1593f4f86221ff6008a62
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-01-30 13:17:12 +0100

    second_one

---
+++ practica_1.2.js
@@ -0,0 +1,20 @@
+//1
+const sumDue =(var1, var2)=>{
+    console.log(var1, var2);
+    }
+    sumDue('Juan', 'Ymedio')  
+    //2
+ ((a)=>{
+    return a + 100;
+    console.log(10); 
+  })
+  
+ var person ={
+    name:'Pier',
+    showName:(name)=>{
+        console.log(name);
+    }
+  }
+  person.showName("Juan");
+
+  //3  https://www.google.com/search?q=javascript+Escribe+una+funci%C3%B3n+creadora+de+objetos+que+realice+instancias+de+una+clase+abstracta.+Inb%C3%B3cala+con+diferentes+definiciones.&rlz=1C1CHBD_esES930ES930&ei=kbTXY_WLL8uYkwXpo564BQ&ved=0ahUKEwi1mJW4ou_8AhVLzKQKHemRB1cQ4dUDCA8&uact=5&oq=javascript+Escribe+una+funci%C3%B3n+creadora+de+objetos+que+realice+instancias+de+una+clase+abstracta.+Inb%C3%B3cala+con+diferentes+definiciones.&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIKCAAQRxDWBBCwAzIKCAAQRxDWBBCwAzIKCAAQRxDWBBCwAzIKCAAQRxDWBBCwAzIKCAAQRxDWBBCwAzIKCAAQRxDWBBCwAzIKCAAQRxDWBBCwAzIKCAAQRxDWBBCwA0oECEEYAEoECEYYAFCZFFijVWDXWGgBcAF4AIABAIgBAJIBAJgBAKABAcgBCMABAQ&sclient=gws-wiz-serp
