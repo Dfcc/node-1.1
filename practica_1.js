cdd662a17f2006115aa99e0a2572c8f342e46cac
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-01-30 12:28:31 +0100

    finished_1_practice

--- practica_1.js
+++ practica_1.js
@@ -1,15 +1,28 @@
+//nivel 1
 const showName=(name)=>{
     console.log(name)
 }
 
 showName('Juan');
 
+// nivel_2
 function ShowYou (pre, last){
   console.log(`Mi nombre es ${pre} y mi apellido es ${last}`)  
 }
 ShowYou('David', 'Flaquer');
 
 let nombre ='Pitilongui';
-
 console.log( 'Hola '+ nombre);
 console.log ( `Hola ${ nombre }`);
+
+//nivel_3
+let number = 10;
+for(let i= 0; i< number; i++){
+    console.log(i);
+}
+
+(function (name) {
+    const saludo='Hola como estas?';
+    
+ console.log(saludo + name);
+})('Felipe');
